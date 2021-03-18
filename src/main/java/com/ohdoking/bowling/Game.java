8a0417ea3b42bcd45985ac194185569b16c82cb2
Kenny Oh <ohdoking@gmail.com>
2021-03-18 06:36:03 +0100

    refactoring scoreForFrame

--- src/main/java/com/ohdoking/bowling/Game.java
+++ src/main/java/com/ohdoking/bowling/Game.java
@@ -3,8 +3,8 @@ package com.ohdoking.bowling;
 public class Game {
 
     private int itsScore = 0;
-    private int[] itsThrows = new int[21];
     private int itsCurrentThrow = 0;
+    private int[] itsThrows = new int[21];
 
     public void add(int pins) {
         itsThrows[itsCurrentThrow++] = pins;
@@ -15,10 +15,14 @@ public class Game {
         return this.itsScore;
     }
 
-    public int scoreForFrame(int frame) {
+    public int scoreForFrame(int theFrame) {
         int score = 0;
-        for(int ball = 0 ; frame > 0 && (ball < itsCurrentThrow); ball += 2, frame--){
-            score += itsThrows[ball] + itsThrows[ball + 1];
+        int ball = 0;
+
+        for(int currentFrame = 0; currentFrame < theFrame; currentFrame++){
+            int firstThrow = itsThrows[ball++];
+            int secondThrow = itsThrows[ball++];
+            score += firstThrow + secondThrow;
         }
         return score;
     }
