0e461845998ca960bfdbff79cc715839aa2e2dba
Kenny Oh <ohdoking@gmail.com>
2021-03-19 08:19:17 +0100

    refactoring for single responsibility principle

--- src/main/java/com/ohdoking/bowling/Game.java
+++ src/main/java/com/ohdoking/bowling/Game.java
@@ -3,81 +3,43 @@ package com.ohdoking.bowling;
 public class Game {
 
     private int itsScore = 0;
-    private int itsCurrentThrow = 0;
-    private int[] itsThrows = new int[21];
+
     private int itsCurrentFrame = 1;
     private boolean firstThrowInFrame = true;
+    private Scorer itsScorer = new Scorer();
 
-    private int ball;
+    public int score() {
+        return scoreForFrame(getCurrentFrame() - 1);
+    }
+
+    public int getCurrentFrame() {
+        return this.itsCurrentFrame;
+    }
 
     public void add(int pins) {
-        itsThrows[itsCurrentThrow++] = pins;
+        this.itsScorer.addThrow(pins);
         this.itsScore += pins;
         adjustCurrentFrame(pins);
     }
 
     private void adjustCurrentFrame(int pins) {
-        if (firstThrowInFrame){
+        if (firstThrowInFrame) {
             // strike
-            if(pins == 10) {
+            if (pins == 10) {
                 itsCurrentFrame++;
-            }
-            else{
+            } else {
                 firstThrowInFrame = false;
             }
-        }
-        else{
+        } else {
             firstThrowInFrame = true;
             this.itsCurrentFrame++;
         }
         itsCurrentFrame = Math.min(11, itsCurrentFrame);
     }
 
-    public int score() {
-        return scoreForFrame(getCurrentFrame() - 1);
-    }
 
     public int scoreForFrame(int theFrame) {
-        int score = 0;
-        ball = 0;
-
-        for(int currentFrame = 0; currentFrame < theFrame; currentFrame++){
-            if (isStrike()){
-                score += 10 + nextTwoBallsForStrike();
-                ball++;
-            } else if(isSpare()){
-                score += 10 + nextBallForSpare();
-                ball += 2;
-            }
-            else{
-                score += twoBallsInFrame();
-                ball += 2;
-            }
-        }
-        return score;
+        return itsScorer.scoreForFrame(theFrame);
     }
 
-    private int nextTwoBallsForStrike() {
-        return itsThrows[ball + 1] + itsThrows[ball + 2];
-    }
-
-    private boolean isStrike() {
-        return itsThrows[ball] == 10;
-    }
-
-    private int nextBallForSpare() {
-        return itsThrows[ball + 2];
-    }
-
-    private boolean isSpare() {
-        return (itsThrows[ball] + itsThrows[ball + 1]) == 10;
-    }
-
-    private int twoBallsInFrame(){
-        return itsThrows[ball] + itsThrows[ball + 1];
-    }
-
-    public int getCurrentFrame() {
-        return this.itsCurrentFrame;
-    }
 }
