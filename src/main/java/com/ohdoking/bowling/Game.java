e4bc2d0f8e556922ddabede5b96d136c40c45dfb
Kenny Oh <ohdoking@gmail.com>
2021-03-19 08:24:40 +0100

    refactoring for readable code

--- src/main/java/com/ohdoking/bowling/Game.java
+++ src/main/java/com/ohdoking/bowling/Game.java
@@ -2,8 +2,6 @@ package com.ohdoking.bowling;
 
 public class Game {
 
-    private int itsScore = 0;
-
     private int itsCurrentFrame = 1;
     private boolean firstThrowInFrame = true;
     private Scorer itsScorer = new Scorer();
@@ -18,25 +16,31 @@ public class Game {
 
     public void add(int pins) {
         this.itsScorer.addThrow(pins);
-        this.itsScore += pins;
         adjustCurrentFrame(pins);
     }
 
     private void adjustCurrentFrame(int pins) {
         if (firstThrowInFrame) {
-            // strike
-            if (pins == 10) {
-                itsCurrentFrame++;
-            } else {
+            if (!adjustFrameForStrike(pins)) {
                 firstThrowInFrame = false;
             }
         } else {
             firstThrowInFrame = true;
-            this.itsCurrentFrame++;
+            advanceFrame();
         }
-        itsCurrentFrame = Math.min(11, itsCurrentFrame);
     }
 
+    private void advanceFrame() {
+        itsCurrentFrame = Math.min(11, itsCurrentFrame + 1);
+    }
+
+    private boolean adjustFrameForStrike(int pins){
+        if (pins == 10) {
+            advanceFrame();
+            return true;
+        }
+        return false;
+    }
 
     public int scoreForFrame(int theFrame) {
         return itsScorer.scoreForFrame(theFrame);
